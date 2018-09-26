43ba890dc920381563db5780979f41e06385cebe
Zach Bryan <zachx1@gmail.com>
2018-09-25 19:12:18 -0700

    fixed notfound

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-const NotFound = () = (
+const NotFound = () => (
   <div>
     <h2>Not Found!!</h2>
   </div>
-)
+);
 export default NotFound;
