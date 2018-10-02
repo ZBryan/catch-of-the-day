067c38f6b6e76cb7c1fd2ce2a3f1a0c840299839
Zach Bryan <zachx1@gmail.com>
2018-10-01 18:20:46 -0700

    rendering default fish

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import Order from "./Order";
+import Fish from "./Fish";
 import Inventory from "./Inventory";
 import sampleFishes from "../sample-fishes";
 
@@ -22,6 +23,11 @@ class App extends Component {
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish key={key} index={key} details={this.state.fishes[key]} />
+            ))}
+          </ul>
         </div>
         <Order />
         <Inventory addFish={this.addFish} loadFish={this.loadSampleFishes} />
